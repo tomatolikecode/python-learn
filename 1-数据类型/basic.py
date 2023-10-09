fb040be2e9b5f1b346475e0aa78bc03b0c920f4b
王方权 <806284259@qq.com>
2023-10-09 10:56:34 +0800

    添加print函数说明

--- 1-数据类型/basic.py
+++ 1-数据类型/basic.py
@@ -1,9 +1,15 @@
+# string  单引号或双引号表示字符串, 反斜杠\转义特殊字符
+# 变量[头下标:尾下标(不取)]
+strA = 'hello1'
+strB = "hello2"
+strC = "\"hello3\""
+print(strA, strB, strC)
+print(strA[1:3])
+print(strA[-3:-1])
 
 
-def hello():
-    print("python 学习之旅")
-
-
-if __name__ == '__main__':
-    hello()
+# bool
+boolA = True
+boolB = False
+print(boolA, boolB)
 
--- 2-流程控制/main.py
+++ 2-流程控制/main.py
@@ -27,18 +27,4 @@ True==1, False==0 会返回 True, 但可以通过 is 来判断类型
 '''
 
 
-# string  单引号或双引号表示字符串, 反斜杠\转义特殊字符
-# 变量[头下标:尾下标(不取)]
-strA = 'hello1'
-strB = "hello2"
-strC = "\"hello3\""
-print(strA, strB, strC)
-print(strA[1:3])
-print(strA[-3:-1])
-
-
-# bool
-boolA = True
-boolB = False
-print(boolA, boolB)
 
